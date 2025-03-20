4921c8dac2b77a807408269c750347e08ecda90a
nice2008 <t09n09a08@gmail.com>
2025-03-21 02:26:03 +0500

    add personalDate model to feedback, fix flake8 conf

--- backend/apps/feedback/models.py
+++ backend/apps/feedback/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+import django.db
+import django.db.models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+__all__ = ["PersonalData"]
+
+
+class PersonalData(django.db.models.Model):
+    user = django.db.models.ForeignKey()
+    name = django.db.models.TextField(
+        _("name"),
+        max_length=150,
+        help_text=_("Feedback author"),
+        blank=True,
+        null=True,
+    )
+    mail = django.db.models.EmailField(
+        _("mail"),
+        help_text=_("Feedback author`s mail"),
+        max_length=200,
+    )
+
+    def __str__(self):
+        return self.mail[:15]
+
+
+class Feedback(django.db.models.Model):
+    
