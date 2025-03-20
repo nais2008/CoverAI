20b66a4f05a9999f0bb6ef22070804415caa42c1
nice2008 <t09n09a08@gmail.com>
2025-03-21 01:57:04 +0500

    add: min style, new components

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css"
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <span className="header__logo">Logo</span>
+    </header>
+  )
+}
+
+export default Header
---
+++ frontend/src/components/Main/Main.tsx
@@ -0,0 +1,16 @@
+import Button from "../Button";
+
+import "./Main.css"
+
+const Main = () => {
+  return (
+    <>
+      <main>
+        Main
+        <Button onClick={() => alert("text")}>Text</Button>
+      </main>
+    </>
+  )
+}
+
+export default Main
